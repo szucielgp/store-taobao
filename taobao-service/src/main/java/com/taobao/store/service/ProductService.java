0a4ea17c7e0e343aaa799aa5917cc6141d4531c6
gavin_lu <gavin_lu@kingdee.com>
2017-04-05 22:46:05 +0800

    Initial commit

---
+++ taobao-service/src/main/java/com/taobao/store/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.taobao.store.service;
+
+/**
+ * Created by kd_gavin on 2017/4/5,22:02.
+ *
+ * @dicription
+ */
+public interface ProductService {
+
+    void test(int i);
+}
---
+++ taobao-service/src/main/java/com/taobao/store/service/impl/ProductServiceImpl.java
@@ -0,0 +1,14 @@
+package com.taobao.store.service.impl;
+
+import com.taobao.store.service.ProductService;
+
+/**
+ * Created by kd_gavin on 2017/4/5,22:04.
+ *
+ * @dicription
+ */
+public class ProductServiceImpl implements ProductService {
+    public void test(int i) {
+        System.out.println(i);
+    }
+}
